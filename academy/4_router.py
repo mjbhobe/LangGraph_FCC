ae912e465893f8b5038a5fc08a0e13fd6a9a2d38
Manish Bhobe <mjbhobe@gmail.com>
2025-06-23 02:49:36 +0000

    Using Anthropic Claude Sonnet LLM

--- academy/4_router.py
+++ academy/4_router.py
@@ -8,10 +8,12 @@ Code shared for learning purposes only!
 """
 
 from dotenv import load_dotenv
+from typing import Union
 from rich.console import Console
 from rich.markdown import Markdown
 from langchain_core.messages import HumanMessage
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_anthropic import ChatAnthropic
 from langgraph.graph import StateGraph, START, END
 from langgraph.graph import MessagesState
 from langgraph.prebuilt import tools_condition, ToolNode
@@ -20,20 +22,61 @@ load_dotenv()
 console = Console()
 
 
-# define a tools function
-def multiply(a: int, b: int) -> int:
-    """function to multiply 2 integers and return the product
+# define my tool functions
+def add(a: Union[int, float], b: Union[int, float]) -> Union[int, float]:
+    """function to add (or sum) 2 numbers (integer or float) and return the result
     Args:
-        a - first integer
-        b - second integer
+        a - first integer (or float)
+        b - second integer (or float)
+    Returns
+        the sum a + b
+    """
+    return a + b
+
+
+def subtract(a: Union[int, float], b: Union[int, float]) -> Union[int, float]:
+    """function to subtract (or take difference of) 2 numbers (integer or float) and return the result
+    Args:
+        a - first integer (or float)
+        b - second integer (or float)
+    Returns
+        the diference a - b
+    """
+    return a - b
+
+
+def multiply(a: Union[int, float], b: Union[int, float]) -> Union[int, float]:
+    """function to multiply 2 numbers (integer or float) and return the result
+    Args:
+        a - first integer (or float)
+        b - second integer (or float)
     Returns
         the product a * b
     """
     return a * b
 
 
-model = ChatGoogleGenerativeAI(
-    model="gemini-2.0-flash",
+def divide(n: Union[int, float], d: Union[int, float]) -> Union[int, float]:
+    """function to divide two numbers and return the result
+    Args:
+        n - the numerator, or first number
+        d - the denominator, or second number
+    Returns
+        the result of n / d
+    """
+    return n / d if d != 0 else float("inf")
+
+
+# model = ChatGoogleGenerativeAI(
+#     model="gemini-2.0-flash",
+#     temperature=0.0,
+#     max_tokens=1024 * 2,
+#     timeout=30,
+#     max_retries=2,
+# )
+
+model = ChatAnthropic(
+    model="claude-sonnet-4-20250514",
     temperature=0.0,
     max_tokens=1024 * 2,
     timeout=30,
@@ -41,7 +84,8 @@ model = ChatGoogleGenerativeAI(
 )
 
 # bind the model with tools
-model_with_tools = model.bind_tools([multiply])
+tools_list = [add, subtract, multiply, divide]
+model_with_tools = model.bind_tools(tools_list)
 
 
 # tool call node
@@ -53,7 +97,7 @@ def llm_inherent_call(message_state: MessagesState) -> MessagesState:
 # build our graph
 builder = StateGraph(MessagesState)
 builder.add_node("llm_inherent_call", llm_inherent_call)
-builder.add_node("tools", ToolNode([multiply]))
+builder.add_node("tools", ToolNode(tools_list))
 builder.add_edge(START, "llm_inherent_call")
 builder.add_conditional_edges("llm_inherent_call", tools_condition)
 builder.add_edge("tools", END)
@@ -64,7 +108,8 @@ console = Console()
 while True:
     console.print("[green] Query: [/green]", end="")
     query = input().strip().lower()
-    if query in ["exit", "quit"]:
+    if query in ["exit", "quit", "bye"]:
+        console.print("[red]Exiting...[/red]")
         break
     messages = [HumanMessage(content=query)]
     messages = graph.invoke({"messages": messages})
