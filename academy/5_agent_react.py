21c57649c285151d1f65d21d8012650e49af1edb
Manish Bhobe <mjbhobe@gmail.com>
2025-07-03 09:24:05 +0530

    New Code examples

--- academy/5_agent_react.py
+++ academy/5_agent_react.py
@@ -1,17 +1,19 @@
 """
-4_router.py - build a basic router, which routes to a tool call or returns
-    a message from an LLM
+5_agent_react.py - configure a basic math agent with react pattern,
+    which should be able to handle cascading math ops, such as 4 *7 / 12 + 45
 
 @author: Manish Bhobe
 My experiments with AI/ML and Gen AI
 Code shared for learning purposes only!
 """
 
+import math, sys
 from dotenv import load_dotenv
 from typing import Union
 from rich.console import Console
 from rich.markdown import Markdown
 from langchain_core.messages import HumanMessage, SystemMessage
+
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_anthropic import ChatAnthropic
 from langgraph.graph import StateGraph, START, END
@@ -25,58 +27,50 @@ console = Console()
 Number = Union[int, float]
 
 
-# define my tool functions
-def add(a: Union[int, float], b: Union[int, float]) -> Union[int, float]:
-    """function to add (or sum) 2 numbers (integer or float) and return the result
+# define basic math ops as tools
+def add(a: Number, b: Number) -> Number:
+    """function to add 2 numbers and return the sum
     Args:
-        a - first integer (or float)
-        b - second integer (or float)
+        a - first number
+        b - second number
     Returns
         the sum a + b
     """
     return a + b
 
 
-def subtract(a: Union[int, float], b: Union[int, float]) -> Union[int, float]:
-    """function to subtract (or take difference of) 2 numbers (integer or float) and return the result
+def subtract(a: Number, b: Number) -> Number:
+    """function to subtract 2 numbers and return the difference
     Args:
-        a - first integer (or float)
-        b - second integer (or float)
+        a - first number
+        b - second number
     Returns
-        the diference a - b
+        the difference a - b
     """
     return a - b
 
 
-def multiply(a: Union[int, float], b: Union[int, float]) -> Union[int, float]:
-    """function to multiply 2 numbers (integer or float) and return the result
+def multiply(a: Number, b: Number) -> Number:
+    """function to multiply 2 numbers and return the product
     Args:
-        a - first integer (or float)
-        b - second integer (or float)
+        a - first number
+        b - second number
     Returns
         the product a * b
     """
     return a * b
 
 
-def divide(n: Union[int, float], d: Union[int, float]) -> Union[int, float]:
-    """function to divide two numbers and return the result
+def divide(n: Number, d: Number) -> Number:
+    """function to divide 2 numbers and return the quotient
     Args:
-        n - the numerator, or first number
-        d - the denominator, or second number
+        n - numerator
+        d - denominator
     Returns
-        the result of n / d
+        the quotient n / d
     """
-    return n / d if d != 0 else float("inf")
+    return (n / d) if d != 0 else math.nan
 
-
-# model = ChatGoogleGenerativeAI(
-#     model="gemini-2.0-flash",
-#     temperature=0.0,
-#     max_tokens=1024 * 2,
-#     timeout=30,
-#     max_retries=2,
-# )
 
 model = ChatAnthropic(
     model="claude-sonnet-4-20250514",
@@ -86,41 +80,48 @@ model = ChatAnthropic(
     max_retries=2,
 )
 
+# model = ChatGoogleGenerativeAI(
+#     model="gemini-2.0-flash",
+#     temperature=0.0,
+#     max_tokens=1024 * 2,
+#     timeout=30,
+#     max_retries=2,
+# )
+
 # bind the model with tools
 tools_list = [add, subtract, multiply, divide]
 model_with_tools = model.bind_tools(tools_list)
 
-sys_message = SystemMessage(
-    content="You are a helpful assistant taskes with performing math on a set of inputs"
+
+# here is my main LLM assistant
+system_message = SystemMessage(
+    "You are a helpful assistant tasked with solving math problems."
 )
 
 
-# tool call node
 def assistant(message_state: MessagesState) -> MessagesState:
     """node to call the LLM with a tool call"""
     return {
-        "messages": [model_with_tools.invoke(sys_message + message_state["messages"])]
+        "messages": [
+            model_with_tools.invoke([system_message] + message_state["messages"])
+        ]
     }
 
 
+console = Console()
+
 # build our graph
 builder = StateGraph(MessagesState)
-# add nodes
 builder.add_node("assistant", assistant)
 builder.add_node("tools", ToolNode(tools_list))
 builder.add_edge(START, "assistant")
-# build structure
 builder.add_conditional_edges("assistant", tools_condition)
-# loop back from tools to assistant
 builder.add_edge("tools", "assistant")
-builder.add_edge("tools", END)
+builder.add_edge("assistant", END)
 graph = builder.compile()
-
-console = Console()
-
-# the biggest problem with this graph is that it will handle
-# only a single multiplication (e.g. What is 7 * 8?), but it
-# cannot cascade multiple multiplications (e.g. What is 7 * 8 * 9?).
+# display as ascii
+# console.print(Markdown(graph.get_graph().ascii()), justify="center")
+print(graph.get_graph().print_ascii())
 
 while True:
     console.print("[green] Query: [/green]", end="")
